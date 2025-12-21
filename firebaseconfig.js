23e1e14e4db53850ad6315cfac929c26e21d2871
José Pedro Rodrigues <jose.rodrigues@c2b.pt>
2025-12-21 18:11:22 +0000

    Alterações do firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvq1ERAyBPDjxYBvB_ws35JSwEW7lPqCM",
+  authDomain: "pap-techweek.firebaseapp.com",
+  projectId: "pap-techweek",
+  storageBucket: "pap-techweek.firebasestorage.app",
+  messagingSenderId: "660685743521",
+  appId: "1:660685743521:web:e12b4ada3e21c6fea617e0"
+};
+ 
+firebase.initializeApp(firebaseConfig);

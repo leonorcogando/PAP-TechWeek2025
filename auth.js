21bb9226d4e0f8bff5005e8c47b725dd758b0e6b
José Pedro Rodrigues <jose.rodrigues@c2b.pt>
2026-01-14 16:22:02 +0000

    ALTERAÇÃO NA AUTENTICAÇÃO LOGIN

--- auth.js
+++ auth.js
@@ -1,16 +1,37 @@
 // LOGIN
+//Para já para dar logout, abrir o f12 e colocar este código:firebase.auth().signOut()
+
 document.getElementById("loginBtn").addEventListener("click", () => {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
 
+  //Retirar isto depois
+  console.log("Email:", email);
+  console.log("Password:", password);
+  //Até aqui
+
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      window.location.href = "main/main.html";
-    })
-    .catch((error) => {
-      alert("Erro: " + error.message);
-    });
+        closeModal('login');
+  })
+  .catch((error) => {
+    alert("Erro: " + error.message);
+  });
 });
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    document.getElementById("userInfo").style.display = "flex";
+    document.getElementById("loginButton").style.display = "none";
+
+    const name = user.displayName || user.email;
+    document.getElementById("userName").textContent = name;
+  } else {
+    document.getElementById("userInfo").style.display = "none";
+    document.getElementById("loginButton").style.display = "block";
+  }
+});
+
 
 // REGISTO
 document.getElementById("registerBtn").addEventListener("click", () => {
